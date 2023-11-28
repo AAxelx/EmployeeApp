b3af40e1a7d6a8438bb1a62ea50766c4be280abd
Anton Kravchenko <78639975+AAxelx@users.noreply.github.com>
2023-11-28 16:48:59 +0200

    Added Service

---
+++ EmployeeApp.SAL/Services/Abstractions/IEmployeesService.cs
@@ -0,0 +1,11 @@
+﻿using EmployeeApp.Common.Models;
+
+namespace EmployeeApp.SAL.Services.Abstractions
+{
+	public interface IEmployeesService
+	{
+        Task<ServiceValueResult<Employee>> GetEmployeeByIdAsync(int id);
+        Task<ServiceValueResult<Employee>> UpdateEmployeeAsync(int id);
+    }
+}
+
---
+++ EmployeeApp.SAL/Services/EmployeesService.cs
@@ -0,0 +1,50 @@
+﻿using EmployeeApp.Common.Enums;
+using EmployeeApp.Common.Models;
+using EmployeeApp.DAL.Repositories.Abstractions;
+using EmployeeApp.SAL.Services.Abstractions;
+
+namespace EmployeeApp.SAL.Services
+{
+	public class EmployeesService : IEmployeesService
+    {
+        private readonly IEmployeesRepository _repository;
+
+		public EmployeesService(IEmployeesRepository repository)
+		{
+            _repository = repository;
+		}
+
+        public async Task<ServiceValueResult<Employee>> GetEmployeeByIdAsync(int id)
+        {
+            var employee = await _repository.GetByIdAsync(id);
+
+            if (employee == null)
+            {
+                return new ServiceValueResult<Employee>(ResponseType.NotFound);
+            }
+
+            return new ServiceValueResult<Employee>(employee);
+        }
+
+        public async Task<ServiceValueResult<Employee>> UpdateEmployeeAsync(int id)
+        {
+            var employee = await _repository.GetByIdAsync(id);
+
+            if (employee == null)
+            {
+                return new ServiceValueResult<Employee>(ResponseType.NotFound);
+            }
+
+            var isSuccess = await _repository.ToggleEmployeeEnableAsync(id);
+
+            if (!isSuccess)
+            {
+                return new ServiceValueResult<Employee>(ResponseType.BadRequest);
+            }
+
+            employee.Enable = !employee.Enable;
+            return new ServiceValueResult<Employee>(employee);
+        }
+    }
+}
+
